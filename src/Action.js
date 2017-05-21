c749f6018bbaaea2f0261433c3839c8123968cee
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-21 23:34:36 +0900

    Modify to apply sample code

--- src/Action.js
+++ src/Action.js
@@ -1,13 +1,41 @@
 "use strict";
 
-import {
-  ADD_TODO,
-  REMOVE_TODO
-} from "./ActionTypes.js";
+/*
+ * action types
+ */
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
 
-// {
-  // type: ADD_TODO,
-  // text: "Build my first Redux app"
-// }
+/*
+ * other constants
+ */
+export const VisibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+};
 
-console.log(ADD_TODO);
+/*
+ * action creators
+ */
+export function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    text: text
+  };
+}
+
+export function toggleTodo(index) {
+  return {
+    type: TOGGLE_TODO,
+    index: index
+  };
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter: filter
+  };
+}
--- src/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-export const ADD_TODO = "ADD_TODO";
-export const REMOVE_TODO = "REMOVE_TODO";
