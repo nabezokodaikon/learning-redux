9d94da5d904fb2b6f47d738a567e79fd993c7f5e
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-22 00:15:52 +0900

    Modify to state shape example

---
+++ src/Reducers.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+ * State shape
+ * {
+ *  visibilityFilter: "SHOW_ALL",
+ *  todos: [
+ *    {
+ *      text: "Consider using Redux",
+ *      complete: true
+ *    },
+ *    {
+ *      text: "Keep all state in a single tree",
+ *      complete: false
+ *    }
+ *  ]
+ * }
+ */
