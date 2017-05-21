678c2b6a06eac677a7963e78fb5b24114e8a6227
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-21 23:00:34 +0900

    Modify to check operation

---
+++ src/action.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import {
+  ADD_TODO,
+  REMOVE_TODO
+} from "./ActionType.js";
+
+// {
+  // type: ADD_TODO,
+  // text: "Build my first Redux app"
+// }
+
+console.log(ADD_TODO);
