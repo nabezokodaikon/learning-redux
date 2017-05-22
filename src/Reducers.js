b15243beba13762aa210df0554afbcbfa7b2320e
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-22 10:33:39 +0900

    Modify to create reducers

--- src/Reducers.js
+++ src/Reducers.js
@@ -17,17 +17,67 @@
  * }
  */
 
-import { VisibilityFilters } from "./Actions.js";
+import { combineReducers } from "redux";
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  VisibilityFilters
+} from "./Actions.js";
 
+const { SHOW_ALL } = VisibilityFilters;
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+function todos(state = [], action) {  
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+          ...state.todos,
+          {
+            text: action.text,
+            complete: false
+          }
+        ];
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+          if (index == action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            });
+          } else {
+            return todo;
+          }
+        });
+    default:
+      return state;
+  }
+}
+
+/*
 const initialState = {
-  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  visibilityFilter: SHOW_ALL,
   todos: []
 };
 
-function todoApp(state, action) {
-  if (typeof state === "undefined") {
-    return initialState;
-  }
-
-  return state;
+function todoApp(state = initialState, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action)
+  };
 }
+*/
+
+const todoApp = combineReducers({
+  visibilityFilter: visibilityFilter,
+  todos: todos
+});
+
+export default todoApp;
