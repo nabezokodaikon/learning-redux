bf706b3487f03267d32ccb59476d0e06cb14dfb0
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-23 08:26:20 +0900

    Modify implementition of AddTodo.jsx
    Change from method definition to class definition.

--- src/containers/AddTodo.jsx
+++ src/containers/AddTodo.jsx
@@ -1,32 +1,42 @@
 "use strict";
 
 import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addTodo } from "../Actions";
 
-let AddTodo = ({ dispatch }) => {
-  let input;
+class AddTodoSrc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.input = null;
+  }
 
-  return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault();
-        if (input.value.trim().length > 0) {
-          dispatch(addTodo(input.value));
-          input.value = "";
-        } else {
-          return;
-        }
-      }}>
-        <input ref={node => input = node} />
-        <button type="submit">
-          Add Todo
-        </button>
-      </form>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <form onSubmit={e => {
+          e.preventDefault();
+          if (this.input.value.trim().length > 0) {
+            this.props.dispatch(addTodo(this.input.value));
+            this.input.value = "";
+          } else {
+            return;
+          }
+        }}>
+          <input ref={node => this.input = node} />
+          <button type="submit">
+            Add Todo
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+AddTodoSrc.propTypes = {
+  dispatch: PropTypes.func.isRequired
 };
 
-AddTodo = connect()(AddTodo);
+const AddTodo = connect()(AddTodoSrc);
 
 export default AddTodo;
