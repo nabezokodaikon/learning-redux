29d5b51a67497d8807713c6f8b6d34cff97f3384
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-22 23:04:45 +0900

    Modify to running on electron

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import todoApp from "./reducers/todoApp";
-import App from "./components/App";
+import todoApp from "./build/reducers/todoApp";
+import App from "./build/components/App";
 
 const store = createStore(todoApp);
 
