197311cde706d28b87e2b3699ecc994d763d4f9c
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-22 21:53:10 +0900

    Modify implementity of index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 "use strict";
 
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 import { createStore } from "redux";
-import todoApp from "./Reducers.js";
+import todoApp from "./Reducers";
+import App from "./components/App";
 
 const store = createStore(todoApp);
 
-// Test code
-// import {
-  // addTodo,
-  // toggleTodo,
-  // setVisibilityFilter,
-  // VisibilityFilters
-// } from "./Actions.js";
-
-// const unsubscribe = store.subscribe(() => {
-  // console.log(store.getState());
-// });
-
-// console.log(store.getState());
-// store.dispatch(addTodo("Lean about actions"));
-// store.dispatch(addTodo("Lean about reducers"));
-// store.dispatch(addTodo("Lean about store"));
-// store.dispatch(toggleTodo(0));
-// store.dispatch(toggleTodo(1));
-// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
