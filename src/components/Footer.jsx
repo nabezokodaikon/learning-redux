385916f78039ca05ef2da8478ff23182068948c4
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-22 20:02:51 +0900

    Modify implemetation of Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+"use strict";
+
+import React from "react";
+import FilterLink from "../containers/FilterLink";
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p>
+        Show:
+        {" "}
+        <FilterLink filter="SHOW_ALL">
+          All
+        </FilterLink>
+        {", "}
+        <FilterLink filter="SHOW_ACTIVE">
+          Active
+        </FilterLink>
+        {", "}
+        <FilterLink filter="SHOW_COMPLETED">
+          Completed
+        </FilterLink>
+      </p>
+    );
+  }
+}
+
+
+export default Footer;
